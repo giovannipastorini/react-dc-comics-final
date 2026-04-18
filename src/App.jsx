09d5fcaf804e3991e8e40b34ee7f9e3e041ab381
giovannipastorini <giovannipastorini9@gmail.com>
2026-04-18 15:29:46 +0200

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+/* 
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from './assets/vite.svg'
+import heroImg from './assets/hero.png' 
+*/
+
+
+function App() {
+  
+
+  return (
+    <>
+      <h1>Ciaoz</h1>
+    </>
+  )
+}
+
+export default App
