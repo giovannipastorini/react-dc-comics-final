7a3c129ae0093f1b7789be5aaf849beeaadaaf3f
giovannipastorini <giovannipastorini9@gmail.com>
2026-04-21 18:23:05 +0200

    commit

--- src/components/ContentArea.jsx
+++ src/components/ContentArea.jsx
@@ -1,10 +1,43 @@
+import comics from "../data/comics"
 
+import Jumbotron from "./Jumbotron"
 
 export default function ContentArea (){
 
     return (
         <div id="main-first-div">
-                <span>{"--> "}Content goes here{" <--"}</span>
+
+                <Jumbotron />
+
+                <div>
+                    <ul className="text-white">
+                        {
+                            comics.map(item =>(
+                                <li key={item.id} >{item.series}</li>
+                            ))
+                        }
+                    </ul>
+                </div>
+
+                <div>
+                    <div className="container">
+                        <div className="row">
+                            {
+                                comics.map ( item => (
+                                    <div className="col-2">
+                                        <div className="card">
+                                            <img src={item.thumb} alt={item.series} />
+                                            <div className="card-body">
+                                                <span>{item.series}</span>
+                                            </div>
+                                        </div>
+                                    </div>
+                                ))
+                            }
+                        </div>
+                    </div>
+
+                </div>
         </div>
     )
 }
---
+++ src/components/Jumbotron.jsx
@@ -0,0 +1,11 @@
+import jumbotronImg from "../../img/jumbotron.jpg"
+
+export default function Jumbotron (){
+
+    return (
+        <div id="jumbotron">
+            <img src={jumbotronImg} alt="jumbotron" />
+        </div>
+        
+    )
+}
