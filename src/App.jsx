fd15e8e7fe303397bc2e03f7a2300bf7ffa99d3e
giovannipastorini <giovannipastorini9@gmail.com>
2026-04-26 12:41:47 +0200

    feat: add AppFooter component and update App layout

--- src/App.jsx
+++ src/App.jsx
@@ -4,20 +4,15 @@ import menu_items from "./data/menu_items"
 //import components:
 import AppHeader from "./components/AppHeader"
 import AppMain from "./components/AppMain"
+import AppFooter from "./components/AppFooter"
 
 function App() {
 
   return (
     <>
-      
       <AppHeader menu_items={menu_items} />
-      <AppMain />
-      
-      <footer>
-        <p>footer</p>
-      </footer>
-
-
+      <AppMain /> 
+      <AppFooter />
     </>
   )
 }
---
+++ src/components/AppFooter.jsx
@@ -0,0 +1,53 @@
+export default function AppFooter (){
+
+    return(
+        <footer>
+            <div className="container">
+                <div className="row">
+                    <div className="col-1">
+                        <ul>
+                            <li>AAAA</li>
+                            <li>AAAA</li>
+                            <li>AAAA</li>
+                            <li>AAAA</li>
+                            <li>AAAA</li>
+                            <li>AAAA</li>
+                            <li>AAAA</li>
+                            <li>AAAA</li>
+                            <li>AAAA</li>
+                            <li>AAAA</li>
+                        </ul>
+                    </div>
+                    <div className="col-1">
+                        <ul>
+                            <li>AAAA</li>
+                            <li>AAAA</li>
+                            <li>AAAA</li>
+                            <li>AAAA</li>
+                            <li>AAAA</li>
+                            <li>AAAA</li>
+                            <li>AAAA</li>
+                            <li>AAAA</li>
+                            <li>AAAA</li>
+                            <li>AAAA</li>
+                        </ul>
+                    </div>
+                    <div className="col-1">
+                        <ul>
+                            <li>AAAA</li>
+                            <li>AAAA</li>
+                            <li>AAAA</li>
+                            <li>AAAA</li>
+                            <li>AAAA</li>
+                            <li>AAAA</li>
+                            <li>AAAA</li>
+                            <li>AAAA</li>
+                            <li>AAAA</li>
+                            <li>AAAA</li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </footer>
+    )
+}
